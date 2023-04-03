38dbb51760236a857286732770e2f65ccbb3c8b2
franciele <francielefernandes126@gmail.com>
2023-04-02 21:36:42 -0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Core\Creational\Example;
+
+
+
+$example = new Example;
+var_dump($example);
---
+++ src/Core/Creational/Example.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Core\Creational;
+
+
+
+class Example{
+    public function __construct(){
+        echo "Hello World";
+    }
+}
